ac0d7aa125c0a954440fa23410327330cd0237fc
Etienne <etienne.kerinckx@yahoo.com>
2018-04-22 14:59:21 +0200

    Initial pipe-based IPC server implementation

---
+++ application/run-server.bat
@@ -0,0 +1,4 @@
+@echo off
+
+set PORT=3000
+start "" npm run watch
--- application/server/start.ts
+++ application/server/start.ts
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 
 import * as http from "http";
+import * as Net from "net";
+import * as Path from "path";
 import * as logger from "morgan";
 
 import Server from "./server";
@@ -11,32 +13,61 @@ class ServerStarter
 	private httpServer : http.Server;
 	private port : string;
 
+	private auxiliaryServer : Server;
+	private ipcServer : Net.Server;
+	private pipe : string;
+
 	private constructor()
 	{
+		// Prepare the primary server instance
 		this.server = Server.create();
 		this.port = process.env.PORT || "8080"; // TODO: port normalization and configuration
 
 		this.server.express.set("port", this.port);
 
+		// Prepare the auxiliary server instance
+		this.auxiliaryServer = Server.create();
+		this.pipe = Path.join('\\\\?\\pipe', process.cwd(), "paquet-ipc"); // TODO: magic
+		console.log("Will open pipe: " + this.pipe);
+
+		//this.auxiliaryServer.express.set("port", this.pipe);
+
+		// Create the HTTP server using the primary server instance
 		this.httpServer = http.createServer(this.server.express);
-		this.httpServer.on("listening", () => this.onListening(this));
-		this.httpServer.on("error", (error) => this.onError(this, error));
+		this.httpServer.on("listening", () => this.onListening(this, this.httpServer));
+		this.httpServer.on("error", (error) => this.onError(this, this.httpServer, error));
+
+		// Create the IPC server using the auxiliary server instance
+		this.ipcServer = Net.createServer();
+		//this.ipcServer = http.createServer(this.auxiliaryServer.express);
+		this.ipcServer.on("listening", () => this.onRawServerListening(this, this.ipcServer));
+		this.ipcServer.on("error", (error) => this.onRawServerError(this, this.ipcServer, error));
 	}
 
 	private start() : void
 	{
-		this.httpServer.listen(this.port); 
+		this.httpServer.listen(this.port);
+		this.ipcServer.listen(this.pipe);
 	}
 
-	private onListening(starter : ServerStarter) : void
+	private onListening(starter : ServerStarter, appServer : http.Server) : void
 	{
-		var bind : HTTPBind = starter.httpServer.address();
+		var bind : HTTPBind = appServer.address();
 		console.log("Listening on " + bind.address + ":" + bind.port);
-		//console.log(this);
-		//console.log("Listening on " + this.address);
 	}
 
-	private onError(starter : ServerStarter, error : Error) : void
+	private onError(starter : ServerStarter, appServer : http.Server, error : Error) : void
+	{
+		throw error;
+	}
+
+	private onRawServerListening(starter : ServerStarter, appServer : Net.Server) : void
+	{
+		var bind : HTTPBind = appServer.address();
+		console.log("Listening on " + bind.address + ":" + bind.port);
+	}
+
+	private onRawServerError(starter : ServerStarter, appServer : Net.Server, error : Error) : void
 	{
 		throw error;
 	}
@@ -57,71 +88,3 @@ class HTTPBind
 }
 
 ServerStarter.start();
-
-/*// Module dependencies.
-var app = require('./app').bootstrap();
-var debug = require('debug')('playground:server');
-var http = require('http');
-
-// Get port from environment and store in Express.
-var port = normalizePort(process.env.PORT || '8080');
-app.set('port', port);
-
-// Create HTTP server.
-var server = http.createServer(app);
-
-// Listen on provided port, on all network interfaces.
-server.listen(port);
-server.on('error', onError);
-server.on('listening', onListening);
-
-// Normalize a port into a number, string, or false.
-function normalizePort(val) {
-  var port = parseInt(val, 10);
-
-  if (isNaN(port)) {
-    // named pipe
-    return val;
-  }
-
-  if (port >= 0) {
-    // port number
-    return port;
-  }
-
-  return false;
-}
-
-// Event listener for HTTP server "error" event.
-function onError(error) {
-  if (error.syscall !== 'listen') {
-    throw error;
-  }
-
-  var bind = typeof port === 'string'
-    ? 'Pipe ' + port
-    : 'Port ' + port;
-
-  // handle specific listen errors with friendly messages
-  switch (error.code) {
-    case 'EACCES':
-      console.error(bind + ' requires elevated privileges');
-      process.exit(1);
-      break;
-    case 'EADDRINUSE':
-      console.error(bind + ' is already in use');
-      process.exit(1);
-      break;
-    default:
-      throw error;
-  }
-}
-
-// Event listener for HTTP server "listening" event.
-function onListening() {
-  var addr = server.address();
-  var bind = typeof addr === 'string'
-    ? 'pipe ' + addr
-    : 'port ' + addr.port;
-  debug('Listening on ' + bind);
-}*/
